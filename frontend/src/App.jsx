78984d5c107be25f00fdc97e989bd1eb137debb8
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-03-05 18:09:01 -0300

    atualização

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
 import BookResults from "./pages/BookResults";
+import NotFound from "./pages/NotFound";
 
 export default () => {
 
@@ -17,6 +18,7 @@ export default () => {
         <Route path="/home" element={<Home />} />
         <Route path="/user/:social_handle" element={<Profile />} />
         <Route path="/books/search/:title" element={<BookResults />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
