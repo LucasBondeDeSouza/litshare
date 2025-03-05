4109e6d0e61a0a2a9ccb3f5492507e5d84b4a693
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-03-05 13:53:56 -0300

    atualização

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import UserProfile from "./pages/UserProfile";
+import Profile from "./pages/Profile";
 import BookResults from "./pages/BookResults";
 
 export default () => {
@@ -15,7 +15,7 @@ export default () => {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/user/:social_handle" element={<UserProfile />} />
+        <Route path="/user/:social_handle" element={<Profile />} />
         <Route path="/books/search/:title" element={<BookResults />} />
       </Routes>
     </Router>
--- frontend/src/pages/Profile/index.jsx
+++ frontend/src/pages/Profile/index.jsx
@@ -1,36 +1,63 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
+import Header from "../../components/Header";
+import Cards from "../../components/Cards";
+import ProfileHeader from "../../components/ProfileHeader";
+import Footer from "../../components/Footer";
 
 export default () => {
-    const [data, setData] = useState({});
-    const userId = localStorage.getItem('userId'); // Recuperando o ID do usuário
+    const { social_handle } = useParams(); // Obtendo o identificador do usuário pela URL
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const userId = localStorage.getItem('userId')
+    const [profileId, setProfileId] = useState()
 
     useEffect(() => {
         if (userId) {
-            getUser();
+            getBooks();
         } else {
             console.error('No user ID found in localStorage');
         }
-    }, []);
+    }, [social_handle, userId]);
 
-    const getUser = async () => {
+    const getBooks = async () => {
         try {
-            const response = await axios.get(`http://localhost:3000/api/clients/home/${userId}`);
+            setIsLoading(true);
+            const response = await axios.get(`http://localhost:3000/api/books/${social_handle}?userId=${userId}`);
             setData(response.data);
         } catch (err) {
             console.error('Error fetching user data:', err);
+        } finally {
+            setIsLoading(false);
         }
     };
 
     return (
         <>
-            <div className="bg-light min-vh-100">
+            <Header />
+            
+            <div className="bg-light min-vh-100 pb-5">
                 <div className="container">
-                    <h1>Username:</h1>
-                    <h2>{data.username || "Loading..."}</h2>
-                    <h2>{data.email || "Loading"}</h2>
+                    <div className="pt-5">
+                        <ProfileHeader 
+                            social_handle={social_handle} 
+                            userId={userId} 
+                            setProfileId={setProfileId} 
+                        />
+
+                        <Cards 
+                            data={data} 
+                            userId={userId} 
+                            profileId={profileId} 
+                            isLoading={isLoading} 
+                            getBooks={getBooks} 
+                        />
+                    </div>
                 </div>
             </div>
+
+            <Footer />
         </>
     );
 };
