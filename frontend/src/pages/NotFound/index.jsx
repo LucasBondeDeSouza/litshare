ea4094055e5ba0fe9c51e69974b3c4e6bde49350
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-03-06 18:35:04 -0300

    atualização

--- frontend/src/pages/NotFound/index.jsx
+++ frontend/src/pages/NotFound/index.jsx
@@ -9,7 +9,7 @@ export default () => {
                     <h1 className="display-1 fw-bold">404</h1>
                     <p className="fs-2 fw-medium mt-4">Oops! Page not found</p>
                     <p className="mt-4 mb-5">The page you're looking for doesn't exist or has been moved.</p>
-                    <a href="/home" className="btn btn-secondary fw-semibold rounded-pill px-4 py-2 custom-btn">
+                    <a href="/" className="btn btn-secondary fw-semibold rounded-pill px-4 py-2 custom-btn">
                         Go Home
                     </a>
                 </div>
