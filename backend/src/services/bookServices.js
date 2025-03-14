09322b14a7e737eb69a52178212596ba1a89e500
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-03-14 16:15:51 -0300

    atualização

--- backend/src/services/bookServices.js
+++ backend/src/services/bookServices.js
@@ -99,21 +99,15 @@ export const getBooksForYou = async (id) => {
         `SELECT u.id AS user_id, u.username, u.social_handle, u.picture, b.id AS book_id, b.title, b.review, b.rating, b.olid,
             CASE WHEN l.user_id IS NOT NULL THEN TRUE ELSE FALSE END AS liked_by_user,
             COALESCE(likes_count.count, 0) AS like_count
-        FROM (
-            SELECT * FROM books
-            WHERE user_id != $1  -- Exclui os livros do usuário logado
-            ORDER BY id DESC
-            LIMIT 52
-        ) b
+        FROM books b
         JOIN users u ON b.user_id = u.id
-        JOIN followers f ON u.id = f.followed_id
         LEFT JOIN likes l ON b.id = l.book_id AND l.user_id = $1
         LEFT JOIN (
             SELECT book_id, COUNT(*) AS count
             FROM likes
             GROUP BY book_id
         ) likes_count ON b.id = likes_count.book_id
-        WHERE f.follower_id = $1
+        WHERE b.user_id != $1
         ORDER BY b.id DESC`,
         [id]
     );
